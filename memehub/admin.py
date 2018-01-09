82661747166bf2916c5a3c34b49f40669b2051ba
RoyVanLeeuwen <royvanleeuwen@live.nl>
2018-01-09 12:55:45 +0100

    fixes
    fiuxed a kof

--- memehub/admin.py
+++ memehub/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
+from memehub import models
+ 
+admin.site.register(models.Meme) #Register the model with the admin
--- memehub/views.py
+++ memehub/views.py
@@ -2,30 +2,26 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Meme
 from django.shortcuts import redirect
-
+from .forms import MemeForm 
 
 def index(request):
     return render(request, 'memehub/index.html')
 
-
-def post_new(request):
+def new_meme(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = MemeForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.save()
-            return redirect('memehub/index.html' )
+            return redirect('newmeme')
     else:
-        form = PostForm()
-    return render(request, 'memehub/post_edit.html', {'form': form})
-
+        form = MemeForm()
+    return render(request, 'memehub/new_meme.html', {'form': form})
 
 def post_remove(request, pk):
     post = get_object_or_404(Meme, pk=pk)
     post.delete()
     return redirect('post_list')
-
-
 
 def post_edit(request, pk):
     post = get_object_or_404(Meme, pk=pk)
