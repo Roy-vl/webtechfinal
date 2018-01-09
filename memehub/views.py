e42e1bcb30fa2d90d7eeef3d89d61e1063672e7e
CFKaligula <35255879+CFKaligula@users.noreply.github.com>
2018-01-09 10:50:37 +0100

    models and views

--- memehub/views.py
+++ memehub/views.py
@@ -6,14 +6,10 @@ from .forms import PostForm
 from django.shortcuts import redirect
 
 
-# Create your views here.
-def index(request):
-    return render(request, 'memehub/index.html')
-
 def post_list(request):
     memes = Meme.objects.order_by('published_date')
     print (memes)
-    return render(request, 'base.html', {'todos': todos} )
+    return render(request, 'memehub/base.html', {'todos': todos} )
 
 
 def post_new(request):
@@ -25,7 +21,7 @@ def post_new(request):
             return redirect('base.html' )
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'memehub/post_edit.html', {'form': form})
 
 
 def post_remove(request, pk):
@@ -35,7 +31,7 @@ def post_remove(request, pk):
 
 def post_detail(request, pk):
     post = get_object_or_404(Meme, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'memehub/post_detail.html', {'post': post})
 
 def post_edit(request, pk):
     post = get_object_or_404(Meme, pk=pk)
