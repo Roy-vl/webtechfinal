ac065599ca56ebb5d25eaf4ada891f954cf93614
CFKaligula <35255879+CFKaligula@users.noreply.github.com>
2018-01-09 11:08:11 +0100

    Add files via upload

--- memehub/models.py
+++ memehub/models.py
@@ -1,39 +1,27 @@
 from django.db import models
 from django.utils import timezone
 
-from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from pygments.lexers import get_lexer_by_name
-from pygments.formatters.html import HtmlFormatter
-from pygments import highlight
-
-class Post(models.Model):
-    author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.title
-
-    def descr(self):
-        return self.text
+    CUTE = 'CU'
+    FUNNY = 'FU'
+    DANK = 'DA'
+    ONLYSMARTPPL = 'OS'
+    CATEGORIES = (
+        (CUTE, 'Cute'),
+        (FUNNY, 'Funny'),
+        (DANK, 'Dank),
+        (ONLYSMARTPPL, 'Only Smart People Will Understand'),
+    )
 
 class Meme(models.Model):
     title = models.CharField(max_length=100)
-    image = models.ImageField()
+    image = models.ImageField(upload_to = './memes')
     published_date = models.DateTimeField(blank=True, null=True)
-    categories = models.CharField()
+    categories = models.CharField( default = 'DA', max_length = 50, choices = CATEGORIES)
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
@@ -47,9 +35,9 @@ class Profile(models.Model):
     avater = models.ImageField()
     fb_link = models.URLField()
     location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-    top_3_cat = models_Charfield(
-    choiches = TEST
+    birth_date = models.DateField(max_length = 2, null=True, blank=True)
+    top_3_cat = models_Charfield(default = 'DA', max_length = 50,
+    choiches = CATEGORIES
     )
 
 @receiver(post_save, sender=User)
@@ -60,15 +48,3 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
-
-
-def save(self, *args, **kwargs):
-    """
-    Use the `pygments` library to create a highlighted HTML
-    representation of the code snippet.
-    """
-    lexer = get_lexer_by_name(self.language)
-    options = self.title and {'title': self.title} or {}
-    formatter = HtmlFormatter(style=self.style, linenos=linenos,
-                              full=True, **options)
-    super(Snippet, self).save(*args, **kwargs)
