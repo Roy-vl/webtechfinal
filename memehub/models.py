b17b92107e28e833fa440e766612a802d761abcc
RoyVanLeeuwen <royvanleeuwen@live.nl>
2018-01-10 20:34:57 +0100

    Add files via upload

--- memehub/models.py
+++ memehub/models.py
@@ -6,40 +6,48 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-class Meme(models.Model):
+class Category(models.Model):
     CATEGORIES = [
         ('CU', 'Cute'),
         ('FU', 'Funny'),
         ('DA', 'Dank'),
         ('OS', 'Only Smart People Will Understand'),
     ]
+    title = models.CharField( default = 'DA', max_length = 50, choices = CATEGORIES)
+	
+    def __str__(self):
+        return self.title
+
+
+class Meme(models.Model):
     title = models.CharField(max_length=100, blank=True)
     image = models.ImageField(upload_to = 'memes')
-    categories = models.CharField( default = 'DA', max_length = 50, choices = CATEGORIES)
+    categories = models.ForeignKey(Category, related_name="cat_of_meme" ,on_delete=models.DO_NOTHING)
 
     class Meta: 
         ordering = ['title']
-    
+
     def __str__(self):
         return self.title
 
 class Profile(models.Model):
-    CATEGORIES = [
-        ('CU', 'Cute'),
-        ('FU', 'Funny'),
-        ('DA', 'Dank'),
-        ('OS', 'Only Smart People Will Understand'),
-    ]
+
     user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
     avater = models.ImageField()
     fb_link = models.URLField()
     seenMemes = models.ManyToManyField('Meme')
-    top_3_cat = models.CharField(
-	    max_length = 50,
-	    choices = CATEGORIES,
-	    default = 'DA',
-    )
-	
+    likes = models.ManyToManyField('Category', through='Like')
+    top_cat = models.ForeignKey(Category, related_name="user_most_liked", on_delete=models.DO_NOTHING)
+
     def __str__(self):
         return self.user.username
-		
+
+class Like(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.DO_NOTHING)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    amount = models.IntegerField()
+    class Meta: 
+        ordering = ['amount']
+    def __str__(self):
+        return self.profile.user.username+self.category.title
+    
--- memehub/urls.py
+++ memehub/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-	path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     url(r'^judge/$', views.judge, name='judge'),
+    url(r'^matches/$', views.matches, name='matches'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^judge/like/(?P<pk>\d+)/$', views.like, name='like'),
+    url(r'^dislike/(?P<pk>\d+)/$', views.dislike, name='dislike'),
 ]
