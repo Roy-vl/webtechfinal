da8d81bcb3b0a9e209ac10ecb03abb056ae6482a
RoyVanLeeuwen <royvanleeuwen@live.nl>
2018-01-10 09:53:11 +0100

    Add files via upload

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 from django.conf.urls import include, url
-from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),\
     path('', include('memehub.urls')),\
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, {'template_name': 'registration/logged_out.html'}, name='logout'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
