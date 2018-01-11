b08750b6448c479f029e51dd8a997109fbd20da4
Peter <35175573+Peterselie420@users.noreply.github.com>
2018-01-11 14:39:16 +0100

    Add files via upload
    This is everything but myvenv gucci gang

--- memehub/forms.py
+++ memehub/forms.py
@@ -1,12 +1,9 @@
 from django import forms
 
-from .models import Meme, Profile
+from .models import Meme
 class MemeForm(forms.ModelForm):
+
     class Meta:
         model = Meme
         fields = ('title', 'image', 'categories')
 
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ('fb_link', 'avater')
--- memehub/urls.py
+++ memehub/urls.py
@@ -1,17 +1,10 @@
 from django.conf.urls import url, include
 from django.urls import path
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     path('accounts/', include('django.contrib.auth.urls')),
     url(r'^judge/$', views.judge, name='judge'),
-    url(r'^matches/$', views.matches, name='matches'),
     url(r'^register/$', views.register, name='register'),
-    url(r'^judge/like/(?P<pk>\d+)/$', views.like, name='like'),
-    url(r'^dislike/(?P<pk>\d+)/$', views.dislike, name='dislike'),
-    url(r'^accounts/profile$', views.update_profile, name='profile'),
 ]
-
